8ae3cfbf9e279fbbeea1ef0b2023d2a4d5bdb7f5
marksreeves <mark@prwb.net>
2015-09-21 13:57:31 +1000

    update JSDoc
    Fixed jsDoc in new module.

--- wcomponents-theme/src/main/js/wc/dom/role.js
+++ wcomponents-theme/src/main/js/wc/dom/role.js
@@ -4,7 +4,7 @@
  * @module
  * @requires module:wc/dom/impliedARIA
  */
-define(["wc/dom/impliedARIA"], /** @param {Object} impliedARIA @ignore */function (impliedARIA){
+define(["wc/dom/impliedARIA"], /** @param {Object} impliedARIA @ignore */ function(impliedARIA) {
 	"use strict";
 
 	/**
@@ -17,15 +17,16 @@ define(["wc/dom/impliedARIA"], /** @param {Object} impliedARIA @ignore */functio
 		/**
 		 * Get the role (or implied role) of an element.
 		 *
+		 * @function module:wc/dom/role.get
 		 * @param {Element} element The element to test.
 		 * @param {boolean} [implied] Include getting implied role if true.
 		 * @returns {?String} The WAI-ARIA role of the element, including its implied role if required.
 		 */
 		this.get = function(element, implied) {
 			var role;
-			if(element && element.nodeType === Node.ELEMENT_NODE) {
+			if (element && element.nodeType === Node.ELEMENT_NODE) {
 				role = element.getAttribute("role");
-				if(implied && !role) {
+				if (implied && !role) {
 					role = impliedARIA.getImpliedRole(element);
 				}
 			}
@@ -34,15 +35,16 @@ define(["wc/dom/impliedARIA"], /** @param {Object} impliedARIA @ignore */functio
 
 		/**
 		 * Test if an element has a WAI-ARIA role.
+		 * @function module:wc/dom/role.has
 		 * @param {Element} element The element to test.
 		 * @param {boolean} implied Should we test if the element has an implied role?
-		 * @returns {Boolean} true if the element has a role (or implied role if implied is true).
+		 * @returns {Boolean} true if the element has a role (or implied role if implied is true.
 		 */
 		this.has = function(element, implied) {
 			var result = false;
-			if(element && element.nodeType === Node.ELEMENT_NODE) {
+			if (element && element.nodeType === Node.ELEMENT_NODE) {
 				result = element.hasAttribute("role");
-				if(implied && !result) {
+				if (implied && !result) {
 					result = !!impliedARIA.getImpliedRole(element);
 				}
 			}
