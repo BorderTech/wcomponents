2993e1d18eb9cfcaf75116f93bf92fc350ca8191
javapod <>
2015-09-28 21:21:58 +1000

    Add unit tests for new ComponentModel API.

---
+++ wcomponents-core/src/test/java/com/github/bordertech/wcomponents/ComponentModel_Test.java
@@ -0,0 +1,74 @@
+package com.github.bordertech.wcomponents;
+
+import java.util.ArrayList;
+import java.util.List;
+import static junit.framework.Assert.fail;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author javapod@github.com
+ * @since release2
+ */
+public class ComponentModel_Test {
+
+	@Test    
+	public void testComponentModel_AddAndRemoveChild() {
+
+		final ComponentModel model = new ComponentModel();
+                final WContainer child = new WContainer();
+
+		model.addChild(child);
+
+		Assert.assertTrue("Expected model to have children", model.hasChildren());
+
+		model.removeChild(child);
+
+		Assert.assertFalse("Expected model to have no children", model.hasChildren());
+	}
+        
+	@Test
+	public void testComponentModel_GetChildren() {
+
+		final ComponentModel model = new ComponentModel();
+                final List<WComponent> children = model.getChildren();
+
+		Assert.assertTrue("Expected children to be empty", children.isEmpty());
+
+		try {
+			children.add(new WContainer());
+			fail("Expected add to throw UnsupportedOperationException.");
+		} catch (UnsupportedOperationException e) {
+
+		}
+	}
+
+	@Test
+	public void testComponentModel_SetChildren() {
+
+		final ComponentModel model = new ComponentModel();
+                final List<WComponent> children = new ArrayList<>();
+
+		Assert.assertTrue("Expected children to be empty", children.isEmpty());
+
+                model.setChildren(children);
+
+		Assert.assertFalse("Expected model to have no children", model.hasChildren());
+
+		model.setChildren(null);
+
+		Assert.assertFalse("Expected model to have no children", model.hasChildren());
+
+		final WContainer child = new WContainer();
+		children.add(child);
+		model.setChildren(children);
+		children.remove(child);
+
+		Assert.assertTrue("Expected model to have children", model.hasChildren());
+                
+		model.removeChild(child);
+
+		Assert.assertFalse("Expected model to have no children", model.hasChildren());
+        }
+}
