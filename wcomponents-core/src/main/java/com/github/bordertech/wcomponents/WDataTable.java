ab4b8e60bf03c872f07fbec5dba1b8d4b3ccd39d
John McGuinness <>
2015-10-06 20:19:41 +1100

    Add getChildren() to Container API

--- wcomponents-core/src/main/java/com/github/bordertech/wcomponents/WDataTable.java
+++ wcomponents-core/src/main/java/com/github/bordertech/wcomponents/WDataTable.java
@@ -364,7 +364,7 @@ public class WDataTable extends WBeanComponent implements Disableable, Container
 			for (int col = 0; col < columnCount; col++) {
 				if (model.isCellEditable(row, col)) {
 					// The actual component is wrapped in a renderer wrapper, so we have to fetch it from that
-					WComponent renderer = ((Container) rowRenderer.getRenderer(col)).getChildAt(0);
+					WComponent renderer = ((Container) rowRenderer.getRenderer(col)).getChildren().get(0);
 
 					if (renderer instanceof DataBound) {
 						Object oldValue = model.getValueAt(row, col);
@@ -1803,6 +1803,11 @@ public class WDataTable extends WBeanComponent implements Disableable, Container
 	@Override // to make public
 	public int getIndexOfChild(final WComponent childComponent) {
 		return super.getIndexOfChild(childComponent);
+	}
+
+	@Override
+	public List<WComponent> getChildren() {
+		return super.getChildren();
 	}
 
 	/**
