09d74693114b25a9bffcc5594f1c847a6d8d9422
John McGuinness <j.mcguinness@icloud.com>
2016-01-10 10:54:23 +1100

    Add initial Alignment and Orientation revisions

---
+++ wcomponents-core/src/main/java/com/github/bordertech/wcomponents/layout/Alignment.java
@@ -0,0 +1,22 @@
+package com.github.bordertech.wcomponents.layout;
+
+/**
+ * This is used to control the horizontal alignment of components.
+ *
+ * @author John McGuinness
+ */
+public enum Alignment
+{
+	/**
+	 * Components should be left aligned.
+	 */
+	LEFT,
+	/**
+	 * Components should be center aligned.
+	 */
+	CENTER,
+	/**
+	 * Components should be right aligned.
+	 */
+	RIGHT
+};
---
+++ wcomponents-core/src/main/java/com/github/bordertech/wcomponents/layout/Orientation.java
@@ -0,0 +1,18 @@
+package com.github.bordertech.wcomponents.layout;
+
+/**
+ * This is used to control the orientation of components.
+ *
+ * @author John McGuinness
+ */
+public enum Orientation
+{
+	/**
+	 * Components should be vertically oriented.
+	 */
+	VERTICAL,
+	/**
+	 * Components should be horizontally oriented.
+	 */
+	HORIZONTAL
+};
