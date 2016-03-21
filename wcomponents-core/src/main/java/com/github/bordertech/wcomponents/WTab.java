264f869dc24fe04aa52a60ebb76aea66759363fc
Jonathan Austin <jonathan.austin@border.gov.au>
2016-03-22 10:54:44 +1100

    Fix WTab in WTabGroup

--- wcomponents-core/src/main/java/com/github/bordertech/wcomponents/WTab.java
+++ wcomponents-core/src/main/java/com/github/bordertech/wcomponents/WTab.java
@@ -170,7 +170,7 @@ public class WTab extends AbstractNamingContextContainer implements Disableable,
 		// If is LAZY or DYNAMIC and is the current trigger, then process the WTabSet for the correct open/closed tabs
 		if ((TabMode.LAZY.equals(getMode()) || TabMode.DYNAMIC.equals(getMode())) && AjaxHelper.
 				isCurrentAjaxTrigger(this)) {
-			WTabSet tabSet = (WTabSet) getParent();
+			WTabSet tabSet = (WTabSet) WebUtilities.getAncestorOfClass(WTabSet.class, this);
 			tabSet.handleRequest(request);
 		}
 	}
