8829dccb2e95ccfec20fdd19548d759ab1d37ca1
marksreeves <mark@marksreeves.com>
2016-06-22 22:30:05 +1000

    Remove useless properties
    They were not supposed to escape into the wild

--- wcomponents-core/src/main/java/com/github/bordertech/wcomponents/util/HtmlSanitizer.java
+++ wcomponents-core/src/main/java/com/github/bordertech/wcomponents/util/HtmlSanitizer.java
@@ -38,17 +38,6 @@ public final class HtmlSanitizer {
 	 */
 	private static final String CONFIG_PARAM = "com.github.bordertech.wcomponents.AntiSamy.config";
 
-	/**
-	 * Hold the errors generated in the most recent sanitizer run. May be useful for logging or profiling.
-	 */
-	private static List<String> errors;
-
-	/**
-	 * The time taken for the most recent sanitizer scan. May be useful for profiling.
-	 */
-	private static double scanTime;
-
-
 	static {
 		String path = Config.getInstance().getString(CONFIG_PARAM,
 				"com/github/bordertech/wcomponents/sanitizers/antisamy-wc.xml");
@@ -83,8 +72,6 @@ public final class HtmlSanitizer {
 	 * @throws Exception thrown if the AntiSamy scan fails.
 	 */
 	public static String sanitize(final String input) throws NullPointerException, Exception {
-		errors = null;
-		scanTime = 0;
 		if (Util.empty(input)) {
 			return input;
 		}
@@ -97,34 +84,17 @@ public final class HtmlSanitizer {
 		try {
 			CleanResults results = ANTISAMY.scan(input);
 
-			errors = results.getErrorMessages();
+			List<String> errors = results.getErrorMessages();
 			if (CollectionUtils.isNotEmpty(errors)) {
-				LOG.debug("Errors encountered while sanitizing HTML input/output: "
+				LOG.info("Errors encountered while sanitizing HTML input/output: "
 						+ StringUtils.join(results.getErrorMessages(), "\n"));
 			}
 
-			scanTime = results.getScanTime();
-			LOG.debug("Sanitization time: " + String.valueOf(scanTime));
+			LOG.debug("Sanitization time: " + String.valueOf(results.getScanTime()));
 			return results.getCleanHTML();
 		} catch (ScanException | PolicyException ex) {
 			LOG.error("Cannot sanitize HTML: ", ex);
 			throw new Exception("Cannot sanitize input HTML", ex);
 		}
-	}
-
-	/**
-	 * Get the errors from the last run. Useful if you want to log or profile them (or even report them to the user).
-	 * @return the errors from the most recent run of the sanitizer
-	 */
-	public static List<String> getErrors() {
-		return errors;
-	}
-
-	/**
-	 * Get the time taken for the last sanitizer scan. Useful for profiling.
-	 * @return the time taken by the scan in the most recent run of the sanitizer
-	 */
-	public static Double getScanTime() {
-		return scanTime;
 	}
 }
--- wcomponents-core/src/test/java/com/github/bordertech/wcomponents/util/HtmlSanitizer_Test.java
+++ wcomponents-core/src/test/java/com/github/bordertech/wcomponents/util/HtmlSanitizer_Test.java
@@ -1,8 +1,6 @@
 package com.github.bordertech.wcomponents.util;
 
-import java.util.List;
 import junit.framework.Assert;
-import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
 
 /**
@@ -32,35 +30,13 @@ public class HtmlSanitizer_Test {
 	}
 
 	@Test
-	public void testSanitizerSafeNoErrors() throws Exception {
-		HtmlSanitizer.sanitize(SIMPLE_HTML);
-		Assert.assertTrue(CollectionUtils.isEmpty(HtmlSanitizer.getErrors()));
-	}
-
-	@Test
 	public void testSanitizerTaintedAttribute() throws Exception {
 		Assert.assertEquals(SIMPLE_HTML, HtmlSanitizer.sanitize(TAINTED_ATTRIBUTE));
 	}
 
 	@Test
-	public void testSanitizerTaintedAttributeHasErrors() throws Exception {
-		HtmlSanitizer.sanitize(TAINTED_ATTRIBUTE);
-		List<String> errors = HtmlSanitizer.getErrors();
-		Assert.assertTrue(CollectionUtils.isNotEmpty(errors));
-		Assert.assertEquals(1, errors.size());
-	}
-
-	@Test
 	public void testSanitizerTaintedStyle() throws Exception {
 		Assert.assertEquals("<p style=\"\">content</p>", HtmlSanitizer.sanitize(TAINTED_STYLE));
-	}
-
-	@Test
-	public void testSanitizerTaintedStyleHasErrors() throws Exception {
-		HtmlSanitizer.sanitize(TAINTED_STYLE);
-		List<String> errors = HtmlSanitizer.getErrors();
-		Assert.assertTrue(CollectionUtils.isNotEmpty(errors));
-		Assert.assertEquals(1, errors.size());
 	}
 
 	// We only allow two styles.
@@ -75,7 +51,6 @@ public class HtmlSanitizer_Test {
 		String input = "<p style=\"text-decoration: all;\">content</p>";
 		String expected = "<p style=\"\">content</p>";
 		Assert.assertEquals(expected, HtmlSanitizer.sanitize(input));
-
 	}
 
 	@Test
@@ -83,7 +58,6 @@ public class HtmlSanitizer_Test {
 		String input = "<p style=\"padding-left: any;\">content</p>";
 		String expected = "<p style=\"\">content</p>";
 		Assert.assertEquals(expected, HtmlSanitizer.sanitize(input));
-
 	}
 
 	// I am not going to test every attribute and element in the config XML.
@@ -158,38 +132,6 @@ public class HtmlSanitizer_Test {
 		String input = "<a href=\"page_here\">Hello</a>";
 		String expected = "Hello";
 		Assert.assertEquals(expected, HtmlSanitizer.sanitize(input));
-	}
-
-	@Test
-	public void testLongHtmlTakesTime() throws Exception {
-		String longHtml = "<div><p id='one' class='dot' style='padding:1em;'>Lorem ipsum dolor sit amet, consectetur "
-				+ "adipiscing elit. Vestibulum eget justo eget ipsum placerat tempor. Nullam at auctor quam. Donec at "
-				+ "ante leo. Proin a eros nunc. Cras euismod, lorem quis viverra porta, nisl orci hendrerit mi, sit "
-				+ "amet hendrerit lectus massa eget eros. Cras semper est in aliquet laoreet. Phasellus volutpat "
-				+ "iaculis ultrices. Sed ac feugiat nibh, at facilisis arcu. Nulla elit ligula, molestie sit amet "
-				+ "mauris ut, placerat gravida arcu.</p><p accesskey='A'>Morbi et ante imperdiet, egestas nunc vitae, "
-				+ "mollis dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis "
-				+ "egestas. Maecenas quis metus in ante dictum ultrices. Maecenas justo mi, euismod laoreet consectetur"
-				+ " sed, porttitor mollis tortor. Ut gravida sapien ac nunc porttitor suscipit. Nulla in nibh sit amet "
-				+ "orci placerat facilisis. Duis vel consequat metus. Vivamus tempus euismod pellentesque. Interdum et "
-				+ "malesuada fames ac ante ipsum primis in faucibus. Aliquam bibendum ut metus eu dignissim.</p>"
-				+ "<p tabindex='0'>Sed sagittis est nisi, eu vulputate ex laoreet a. Aliquam luctus, orci ut lobortis "
-				+ "sagittis, arcu odio imperdiet ante, quis mollis massa dolor elementum elit. Integer sed dui sed "
-				+ "metus dapibus elementum nec a enim. Suspendisse nec commodo ex. Vivamus faucibus ante sed sem "
-				+ "condimentum eleifend vitae et sapien. Nullam ultrices libero id nunc pretium commodo. Nullam "
-				+ "posuere elit bibendum fringilla maximus.</p><p role='tab'>Mauris nec aliquet nulla. Sed sed "
-				+ "fermentum odio. Cras ut lacus gravida, tempus turpis ac, rhoncus elit. In dictum dui libero, quis "
-				+ "tempor mi aliquam ut. Ut fringilla venenatis scelerisque. Suspendisse eu orci vel nisl tempor "
-				+ "vehicula eu non ligula. Nam sit amet urna eu turpis tempus imperdiet.</p><p lang='en-gb'>Praesent "
-				+ "in turpis est. Etiam sit amet ultrices sapien. Quisque rutrum porta vulputate. Proin pulvinar, nibh"
-				+ " feugiat accumsan dapibus, nisl mauris tempor libero, a blandit massa ipsum sed lacus. Fusce id elit"
-				+ " non eros laoreet mollis. Aliquam consectetur ligula et rhoncus mattis. Fusce posuere neque id ante "
-				+ "sodales sagittis. Aenean suscipit consectetur felis ut pulvinar. Phasellus dapibus justo eget "
-				+ "sodales mattis. Quisque commodo sem quis lectus convallis, facilisis lacinia odio vehicula. In a "
-				+ "justo sapien.</p></div><ul onclick='alert();'><li class='dot'>one</li><li class='dot'>two</li>"
-				+ "<li class='dot'>three</li><li class='dot'>for</li><li class='dot'>five</li></ul>";
-		HtmlSanitizer.sanitize(longHtml);
-		Assert.assertTrue(0 < HtmlSanitizer.getScanTime());
 	}
 
 	@Test
