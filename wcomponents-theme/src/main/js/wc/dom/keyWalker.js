63fbcc63a6be8415720a6a5a42723c56445ff6d7
marksreeves <mark@marksreeves.com>
2016-11-24 09:08:43 +1100

    KeyWalker unit tests and exception handling

--- wcomponents-theme/src/main/js/wc/dom/keyWalker.js
+++ wcomponents-theme/src/main/js/wc/dom/keyWalker.js
@@ -4,11 +4,13 @@
  */
 define(function() {
 	"use strict";
+
 	var
 		/**
 		 * An object holding property names used in keyWalker configuration.
+		 *
 		 * @constant
-		 * @type {module:wc/dom/keyWalker~options}
+		 * @type {module:keywalker~options}
 		 * @private
 		 */
 		OPTIONS = {
@@ -17,8 +19,9 @@ define(function() {
 		},
 		/**
 		 * An object holding move locations and their value for bitwise calculations.
+		 *
 		 * @constant
-		 * @type {module:wc/dom/keyWalker~moveTo}
+		 * @type {module:keywalker~moveTo}
 		 * @private
 		 */
 		MOVE_TO = {
@@ -35,106 +38,104 @@ define(function() {
 
 	/**
 	 * @constructor
-	 * @alias module:wc/dom/keyWalker~KeyWalker
+	 * @alias module:keywalker~KeyWalker
 	 * @private
 	 */
-	function KeyWalker() {
+	function KeyWalker () {
+		/** @property {String} version the module version, just in case you want to know. */
+		this.version = "1.0.0";
+
 		/**
-		 * Helper for navigating between elements in a tree structure (for example a menu). This function will only
-		 * find the required target element (if any). What you do with that knowledge is up to you!
+		 * This is a simple TreeWalker filter for traversing elements and accepting **anything** unless it is disabled or hidden. You probably don't
+		 * want to use this but it will be used if your configuration object does not include a filter function.
 		 *
 		 * @function
 		 * @private
-		 * @param {module:wc/dom/keyWalker~config} conf The configuration object for this navigation.
-		 * @param {Element} element where we are starting.
-		 * @param {integer} whichWay value from {@link module:wc/dom/keyWalker.MOVE_TO}.
-		 * @returns {?Element} The destination element, if any.
-		 * @throws {ReferenceError} If the value of whichWay is not a known value in {@link module:wc/dom/keyWalker.MOVE_TO}.
+		 * @param {Element} element the element which the TreeWalker instance is investigating
+		 * @return {number} a NodeFilter filter value
 		 */
-		function treeWalkerNavHelper(conf, element, whichWay) {
-			var action,
-				tw,
-				sibling,
-				result = null;
-			if (whichWay && element) {
-				tw = getTreewalker(conf);
-				tw.currentNode = element;
-				action = getAction(conf, element, whichWay);
-				if (action.next && tw[action.next]) {
-					sibling = tw[action.next]();
-					// we may not have a sibling if we are at the ends of a list so cycle unless specifically prevented
-					if (!sibling && conf[OPTIONS.CYCLE] && (whichWay & (MOVE_TO.PREVIOUS | MOVE_TO.NEXT))) {
-						result = treeWalkerNavHelper(conf, action.current, ((whichWay === MOVE_TO.PREVIOUS) ? MOVE_TO.LAST : MOVE_TO.FIRST));
-					}
-					else {
-						result = sibling;
-					}
-				}
+		function simpleFilter (element) {
+			if (element.hasAttribute("disabled") || element.hasAttribute("hidden") || element.getAttribute("aria-disabled") === "true" ||
+				element.getAttribute("aria-hidden") === "true") {
+				return NodeFilter.FILTER_REJECT;
 			}
-			return result;
+			return NodeFilter.FILTER_ACCEPT;
 		}
 
-		/*
-		 * Helper for treeWalkerNavHelper
+		/**
+		 * Find:
+		 *   1. the function name of the appropriate function to pass to a TreeWalker instance for traversal in any allowed direction;
+		 *   2. a node to pass to a recursion of treeWalkerNavHelper if the walk cycles.
+		 *
+		 * This is a helper for treeWalkerNavHelper.
+		 *
 		 * @function
 		 * @private
+		 * @param {module:keywalker~config} conf the configuration object for this navigation
+		 * @param {Element} current the element being tested
+		 * @param {number} whichWay the direction we are going
+		 * @return {Object} func: String, node: ?Element
 		 */
 		function getAction(conf, current, whichWay) {
 			var result = {
-				next: "",
-				current: current
+				func: "",
+				node: current
 			};
 			switch (whichWay) {
 				case MOVE_TO.PARENT:
-					result.next = "parentNode";
+					result.func = "parentNode";
 					break;
 				case MOVE_TO.TOP:
 					conf.tw.currentNode = conf.root;
-					result.next = "firstChild";
+					result.func = "firstChild";
 					break;
 				case MOVE_TO.END:
 					conf.tw.currentNode = conf.root;
-					result.next = "lastChild";
+					result.func = "lastChild";
 					break;
 				case MOVE_TO.PREVIOUS:
-					result.next = conf[OPTIONS.DEPTH_FIRST] ? "previousNode" : "previousSibling";
+					result.func = conf[OPTIONS.DEPTH_FIRST] ? "previousNode" : "previousSibling";
 					break;
 				case MOVE_TO.NEXT:
-					result.next = conf[OPTIONS.DEPTH_FIRST] ? "nextNode" : "nextSibling";
+					result.func = conf[OPTIONS.DEPTH_FIRST] ? "nextNode" : "nextSibling";
 					break;
 				case MOVE_TO.FIRST:
-					result.current = (conf.tw.currentNode = current.parentNode);
-					result.next = "firstChild";
+					result.node = (conf.tw.currentNode = current.parentNode);
+					result.func = "firstChild";
 					break;
 				case MOVE_TO.LAST:
-					result.current = (conf.tw.currentNode = current.parentNode);
-					result.next = "lastChild";
+					result.node = (conf.tw.currentNode = current.parentNode);
+					result.func = "lastChild";
 					break;
 				case MOVE_TO.CHILD:
-					result.next = "firstChild";
+					result.func = "firstChild";
 					break;
 				case MOVE_TO.LAST_CHILD:
-					result.next = "lastChild";
+					result.func = "lastChild";
 					break;
 				default:
-					throw new ReferenceError("keyWalker.treeWalkerNavHelper cannot move to where you think you want to move to.");
+					throw new ReferenceError("Unknown direction for traversing a node tree.");
 			}
 			return result;
 		}
 
-		/*
-		 * Helper for treeWalkerNavHelper
+		/**
+		 * Get a TreeWalker instance based on a particular configuration. Helper for treeWalkerNavHelper.
+		 *
 		 * @function
 		 * @private
+		 * @param {module:keywalker~config} conf the configuration object for this navigation
+		 * @return an instance of TreeWalker
 		 */
 		function getTreewalker(conf) {
-			var tw = conf.tw || (conf.tw = document.createTreeWalker(conf.root, NodeFilter.SHOW_ELEMENT, conf.filter, false));
+			var filter = conf.filter || simpleFilter,
+				tw = conf.tw || (conf.tw = document.createTreeWalker(conf.root, NodeFilter.SHOW_ELEMENT, filter, false));
 			return tw;
 		}
 
 		/**
-		 * Helper for groupBasedNavHelperLoopHelper (so a helper's helper's helper). This is to abstract cycling and
-		 * reduce the paths though what are otherwise extremely complex functions.
+		 * Helper for groupBasedNavHelperLoopHelper (so a helper's helper's helper). This is to abstract cycling and reduce the paths though what are
+		 * otherwise extremely complex functions.
 		 *
 		 * @function
 		 * @private
@@ -164,6 +165,38 @@ define(function() {
 		}
 
 		/**
+		 * Helper for navigating between elements in a tree structure (for example a menu). This function will only find the required target element
+		 * (if any). What you do with that knowledge is up to you!
+		 *
+		 * @function
+		 * @private
+		 * @param {module:keywalker~config} conf the configuration object for this navigation.
+		 * @param {Element} element where we are starting
+		 * @param {integer} whichWay value from {@link module:keywalker.MOVE_TO}
+		 * @returns {?Element} the destination element
+		 * @throws {ReferenceError} if the value of whichWay is not a known value in {@link module:keywalker.MOVE_TO}
+		 */
+		function treeWalkerNavHelper(conf, element, whichWay) {
+			var action,
+				tw,
+				target;
+			if (whichWay && element) {
+				tw = getTreewalker(conf);
+				tw.currentNode = element;
+				action = getAction(conf, element, whichWay);
+				if (action.func && tw[action.func]) {
+					target = tw[action.func]();
+					// we may not have a sibling if we are at the ends of a list so cycle unless specifically prevented
+					if (!target && action.node && conf[OPTIONS.CYCLE] && (whichWay & (MOVE_TO.PREVIOUS | MOVE_TO.NEXT))) {
+						return treeWalkerNavHelper(conf, action.node, ((whichWay === MOVE_TO.PREVIOUS) ? MOVE_TO.LAST : MOVE_TO.FIRST));
+					}
+					return target;
+				}
+			}
+			return null;
+		}
+
+		/**
 		 * Helper for groupBasedNavHelper to bring the do loop into a _slightly_ more readable form.
 		 *
 		 * @function
@@ -174,13 +207,13 @@ define(function() {
 		 * @returns {?Element} The key walk target element if any.
 		 */
 		function groupBasedNavHelperLoopHelper(conf, element, whichWay) {
-
 			var target,
 				cycled = false,
 				direction = whichWay, // so as to not change the arg if we are going to an extremity.
 				group = conf.root,
 				currentIndex = Array.prototype.indexOf.call(group, element),
-				moveHelperObj;
+				moveHelperObj,
+				filter = conf.filter || simpleFilter;
 			do {
 				switch (direction) {
 					case MOVE_TO.NEXT:
@@ -204,7 +237,7 @@ define(function() {
 						break;
 				}
 			}
-			while (target && conf.filter(target) === NodeFilter.FILTER_REJECT);
+			while (target && filter(target) === NodeFilter.FILTER_REJECT);
 			return target;
 		}
 
@@ -215,15 +248,15 @@ define(function() {
 		 *
 		 * @function
 		 * @private
-		 * @param {module:wc/dom/keyWalker~config} conf The configuration object for this navigation.
+		 * @param {module:keywalker~config} conf the configuration object for this navigation.
 		 * @param {Element} element The start point.
-		 * @param {integer} whichWay value from {@link module:wc/dom/keyWalker.OVE_TO}.
+		 * @param {integer} whichWay value from {@link module:keywalker.OVE_TO}.
 		 * @returns {?Element} The destination element, if any.
 		 * @throws {ReferenceError} If the value of whichWay is one reserved for two domensional navigation.
 		 */
 		function groupBasedNavHelper(conf, element, whichWay) {
 			if (whichWay === MOVE_TO.PARENT || whichWay === MOVE_TO.CHILD || whichWay === MOVE_TO.LAST_CHILD) {
-				throw new ReferenceError("Groups have no notion of parent or child, you probably don't want to do this!");
+				throw new ReferenceError("Direction not supported for grouped elements.");
 			}
 
 			if (whichWay && conf.root && conf.root.length) { // the group we are traversing is defined in conf.root
@@ -234,15 +267,15 @@ define(function() {
 
 		/**
 		 * Publicise the map of locations to move to.
-		 * @constant module:wc/dom/keyWalker.MOVE_TO
-		 * @type {module:wc/dom/keyWalker~moveTo}
+		 * @constant module:keywalker.MOVE_TO
+		 * @type {module:keywalker~moveTo}
 		 */
 		this.MOVE_TO = MOVE_TO;
 
 		/**
 		 * Publicise the property names used in a keyWalker configuration object.
-		 * @constant module:wc/dom/keyWalker.OPTIONS
-		 * @type {module:wc/dom/keyWalker~options}
+		 * @constant module:keywalker.OPTIONS
+		 * @type {module:keywalker~options}
 		 */
 		this.OPTIONS = OPTIONS;
 
@@ -250,30 +283,38 @@ define(function() {
 		 * Get the destination. This function will only find the required target element (if any). What you do with
 		 * that knowledge is up to you!
 		 *
-		 * @function module:wc/dom/keyWalker.getTarget
-		 * @param {module:wc/dom/keyWalker~config} conf The configurationfor this particular walk.
+		 * @function module:keywalker.getTarget
+		 * @param {module:keywalker~config} conf the configurationfor this particular walk.
 		 * @param {Element} element The element we are on at the start of the navigation.
-		 * @param {integer} direction The direction to hunt in as a property of {@link module:wc/dom/keyWalker.MOVE_TO}.
+		 * @param {integer} direction The direction to hunt in as a property of {@link module:keywalker.MOVE_TO}.
 		 * @returns {?Element} The end point of navigation (though we do not actually do the navigation here).
 		 */
 		this.getTarget = function (conf, element, direction) {
-			var result = null;
+			if (typeof conf !== "object") {
+				throw new TypeError("conf must be an object.");
+			}
+			var root = conf.root;
 
-			if (conf.root.nodeType) {
-				result = treeWalkerNavHelper(conf, element, direction);
+			if (!root) {
+				throw new TypeError("conf.root must be an element, NodeList or array of elements.");
 			}
-			else {
-				result = groupBasedNavHelper(conf, element, direction);
+
+			if (root.nodeType) {
+				if (root.nodeType === Node.ELEMENT_NODE) {
+					return treeWalkerNavHelper(conf, element, direction);
+				}
+				throw new TypeError("conf.root must be an element node to traverse a tree.");
 			}
-			return result;
+			return groupBasedNavHelper(conf, element, direction);
 		};
 	}
-	return /** @alias module:wc/dom/keyWalker */ new KeyWalker();
+	return /** @alias module:keywalker */ new KeyWalker();
 
 
 	/**
 	 * An object holding move locations and their value for bitwise calculations.
-	 * @typedef {Object} module:wc/dom/keyWalker~moveTo
+	 *
+	 * @typedef {Object} module:keywalker~moveTo
 	 * @property {number} FIRST Move to the first item in the group.
 	 * @property {number} LAST Move to the last item in the group.
 	 * @property {number} PREVIOUS Move to the previous item in the group.
@@ -286,25 +327,23 @@ define(function() {
 	 */
 
 	/**
-	 * Configuration object used by keywalkers.
-	 * @typedef {Object} module:wc/dom/keyWalker~config
-	 * @property {Function} filter A function used to determine a valid target match.
-	 * @property {(Element|NodeList|Array)} root The key walker root; if this is a single element we assume a tree
-	 *    walk otherwise it is a group.
-	 * @property {Boolean} [depthFirst] If true then treeWalker uses depther first in determiing the next node, not
-	 *    used in group based key walking.
-	 * @property {Boolean} [cycle] Set to true if nextSibling on last in branch/group or previousSibling on first in
-	 *    branch/group results in cycling to the first/last respectively.
+	 * Configuration object.
+	 *
+	 * @typedef {Object} module:keywalker~config
+	 * @property {(Element|NodeList|Array<Element>)} root The key walker root; if this is a single element we assume a tree walk otherwise it is
+	 * deemed to be a group.
+	 * @property {Function} filter a TreeWalker filter function used to determine a valid target match.
+	 * @property {Boolean} [depthFirst] If truethy then treeWalker uses depther first in determiing the next node, not used in group based key walking.
+	 * @property {Boolean} [cycle] if truethy nextSibling on last in branch/group or previousSibling on first in branch/group results in cycling to
+	 *  the first/last respectively.
 	 */
 
 	/**
-	 * An object holding property names used in keyWalker configuration. This is publicised as an aide-memoire to
-	 * consuming classes.
+	 * An object holding property names used in keyWalker configuration. This is publicised as an aide-memoire to consuming modules.
 	 *
-	 * @typedef {Object} module:wc/dom/keyWalker~options
+	 * @typedef {Object} module:keywalker~options
 	 * @property {String} CYCLE "cycle" used to set the config.cycle Boolean property.
 	 * @property {String} DEPTH_FIRST "depthFirst" used to set the config.depthFirst Boolean property.
-	 * @see {@link module:wc/dom/keyWalker~config}
+	 * @see {@link module:keywalker~config}
 	 */
-
 });
