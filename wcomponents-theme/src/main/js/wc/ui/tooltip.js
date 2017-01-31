b66791e04fe64fa7590ff6fe95c7be6e5106fec0
marksreeves <mark@marksreeves.com>
2017-01-31 16:40:00 +1100

    Remove useless accesskey underlines

--- wcomponents-theme/src/main/js/wc/ui/tooltip.js
+++ wcomponents-theme/src/main/js/wc/ui/tooltip.js
@@ -1,21 +1,16 @@
-define(["wc/dom/classList", "wc/dom/event", "wc/dom/initialise", "wc/dom/shed", "wc/dom/Widget", "wc/timers", "wc/dom/textContent"],
-	function(classList, event, initialise, shed, Widget, timers, textContent) {
+define(["wc/dom/event", "wc/dom/initialise", "wc/dom/shed", "wc/dom/Widget", "wc/timers"],
+	function(event, initialise, shed, Widget, timers) {
 		"use strict";
 		/**
 		 * @constructor
 		 * @alias module:wc/ui/tooltip~Tooltip
 		 * @private
-		 * @todo document private members.
 		 */
 		function Tooltip() {
 			var showing,
 				isOpen = false,
 				TOOLTIP_TTL = 5000,
-				SHOW_UNDERLINE_CLASS = "wc_alt",
-				TOOLTIP_ELEMENT = "span",
-				TOOLTIPS = new Widget(TOOLTIP_ELEMENT, "", {"role": "tooltip"}),
-				KEYED_ELEMENTS = [new Widget("", "", {accesskey: null}), new Widget("", "", {"data-wc-accesskey": null})],
-				UNDERLINE = new Widget("span", "wc_accesskey");
+				TOOLTIPS = new Widget("span", "", {"role": "tooltip"});
 
 			/*
 			 * Manages scheduled cleanups of any tooltips being shown - they will be
@@ -51,104 +46,23 @@ define(["wc/dom/classList", "wc/dom/event", "wc/dom/initialise", "wc/dom/shed", 
 						shed[showHide](next, true);
 					}
 				}
-				if (hide) {
-					removeAccessLetter();
-				}
-				else {
-					showAllUnderlines();
-				}
 				scheduledCleanup(hide);
 			}
 
-			/**
-			 * Helper for toggleTooltips.
-			 * @private
-			 * @function
-			 */
-			function showAllUnderlines() {
-				var body = document.body;
-
-				Array.prototype.forEach.call(Widget.findDescendants(body, KEYED_ELEMENTS), function (next) {
-					if (!shed.isDisabled(next)) {
-						underlineAccessLetter(next);
-					}
-				});
-				classList.add(body, SHOW_UNDERLINE_CLASS);
-			}
-
 			function keydownEvent($event) {
-				if (!$event.defaultPrevented && !isOpen && $event.keyCode === KeyEvent.DOM_VK_ALT && !($event.repeat)/* won't detect repeat in FF*/) {
+				if (!$event.defaultPrevented && !isOpen && $event.keyCode === KeyEvent.DOM_VK_ALT && !($event.repeat)) {
 					isOpen = true;
 					toggleTooltips(false);
 				}
 			}
 
+			/* NOTE IE8 does not fire keyup when releasing ALT if there is a previous keyup (such as releasing the accesskey key) and if the browser
+			 * native accesskey functionality (for menus) has been invoked.
+			 */
 			function keyupEvent($event) {
-				if (isOpen && !$event.defaultPrevented) {// && $event.keyCode == KeyEvent.DOM_VK_ALT)  // IE8 does not fire keyup when releasing ALT if there is a previous keyup (such as releasing the accesskey key) and if the browser native accesskey functionality (for menus) has been invoked
+				if (isOpen && !$event.defaultPrevented) {
 					toggleTooltips(true);
 				}
-			}
-
-			/*
-			 * Find the first letter matching the access key and underline it. Only look in text node children.
-			 * @param element
-			 * @returns Boolean, true if an access key letter has been underlined
-			 */
-			function underlineAccessLetter(element) {
-				var textNode,
-					content,
-					parent,
-					underlineElement,
-					letter = element.accessKey,
-					position,
-					tw;
-				/*
-				 * A treewalker filter to get child text nodes
-				 * @returns integer
-				 */
-				function childFilter(textNode) {
-					var rval = NodeFilter.FILTER_SKIP,
-						content = textNode.nodeValue;
-
-					if (TOOLTIPS.findAncestor(textNode)) {
-						rval = NodeFilter.FILTER_REJECT;
-					}
-					else if (content.toLocaleUpperCase().indexOf(letter) > -1) {
-						rval = NodeFilter.FILTER_ACCEPT;
-					}
-					return rval;
-				}
-
-				if (letter && !(UNDERLINE.findDescendant(element)) && (content = textContent.get(element)) && content.toLocaleUpperCase().indexOf(letter) > -1) {
-					tw = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, childFilter, false);
-
-					textNode = null;
-					if (tw) {
-						tw.currentNode = element;
-						tw.nextNode();
-						textNode = tw.currentNode;
-					}
-
-					if (textNode && textNode.nodeType === Node.TEXT_NODE) {
-						content = textNode.nodeValue;
-						if ((position = content.toLocaleUpperCase().indexOf(letter)) > -1) {
-							parent = textNode.parentNode;
-							underlineElement = document.createElement(TOOLTIP_ELEMENT);
-							underlineElement.className = "wc_accesskey";
-							underlineElement.innerHTML = content.substr(position, 1);
-
-							if (position !== 0) {
-								parent.insertBefore(document.createTextNode(content.substr(0, position)), textNode);
-							}
-							parent.insertBefore(underlineElement, textNode);
-							textNode.nodeValue = content.substring(position + 1);
-						}
-					}
-				}
-			}
-
-			function removeAccessLetter() {
-				classList.remove(document.body, SHOW_UNDERLINE_CLASS);
 			}
 
 			/**
@@ -177,13 +91,11 @@ define(["wc/dom/classList", "wc/dom/event", "wc/dom/initialise", "wc/dom/shed", 
 		 * is shown when the ALT/META key is pressed.
 		 *
 		 * @module
-		 * @requires module:wc/dom/classList
 		 * @requires module:wc/dom/event
 		 * @requires module:wc/dom/initialise
 		 * @requires module:wc/dom/shed
 		 * @requires module:wc/dom/Widget
 		 * @requires module:wc/timers
-		 * @requires module:wc/dom/textContent
 		 */
 		var instance = new Tooltip();
 		initialise.register(instance);
