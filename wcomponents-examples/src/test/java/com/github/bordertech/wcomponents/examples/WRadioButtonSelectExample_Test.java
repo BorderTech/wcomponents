f889141fea0b84a95c7f6a52c7ba63c2c4397dc6
Jonathan Austin <jonathan.austin@border.gov.au>
2017-05-04 15:42:03 +1000

    Selenium cleanup

--- wcomponents-examples/src/test/java/com/github/bordertech/wcomponents/examples/WRadioButtonSelectExample_Test.java
+++ wcomponents-examples/src/test/java/com/github/bordertech/wcomponents/examples/WRadioButtonSelectExample_Test.java
@@ -1,10 +1,12 @@
 package com.github.bordertech.wcomponents.examples;
 
 import com.github.bordertech.wcomponents.examples.theme.WRadioButtonSelectExample;
+import com.github.bordertech.wcomponents.test.selenium.MultiBrowserRunner;
 import com.github.bordertech.wcomponents.test.selenium.driver.SeleniumWComponentsWebDriver;
 import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 /**
  * Selenium unit tests for {@link WRadioButtonSelectExample}.
