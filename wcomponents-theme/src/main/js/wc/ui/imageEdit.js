BorderTech/wcomponents

commit 3ed822dd90500d8789c2ebbe82b790711fae10db

Sass and icon cleanup (#1139)
* Refactor Sass to allow easier future partial loading of CSS: I removed a few unused rules but it is mostly just a big refactor.
* Optimised the CSS build slightly.
* add a generic border class
* Update menu item selection fix #1133
* fix #1134
* Fix #1137 submenu sticking open
* Move icons into elements for a11y Fix #1136
* Fix #1135 click event failures

* remove unused Sass

* remove unused mixins

wcomponents-theme/src/main/js/wc/ui/imageEdit.js
@@ -1,7 +1,8 @@
-define(["wc/has", "wc/mixin", "wc/dom/event", "wc/dom/uid", "wc/dom/classList", "wc/timers", "wc/ui/prompt",
+define(["wc/has", "wc/mixin", "wc/dom/Widget", "wc/dom/event", "wc/dom/uid", "wc/dom/classList", "wc/timers", "wc/ui/prompt",
 	"wc/loader/resource", "wc/i18n/i18n", "fabric", "wc/ui/dialogFrame", "wc/template", "wc/ui/ImageCapture", "wc/ui/ImageUndoRedo"],
-function(has, mixin, event, uid, classList, timers, prompt, loader, i18n, fabric, dialogFrame, template, ImageCapture, ImageUndoRedo) {
-	var timer, imageEdit = new ImageEdit();
+function(has, mixin, Widget, event, uid, classList, timers, prompt, loader, i18n, fabric, dialogFrame, template, ImageCapture, ImageUndoRedo) {
+	var timer,
+		imageEdit = new ImageEdit();
 
 	ImageEdit.prototype.renderCanvas = function(callback) {
 		if (timer) {
@@ -40,7 +41,8 @@ function(has, mixin, event, uid, classList, timers, prompt, loader, i18n, fabric
 			overlayUrl,
 			undoRedo,
 			registeredIds = {},
-			fbCanvas;
+			fbCanvas,
+			BUTTON = new Widget("button");
 
 
 		function getDialogFrameConfig(onclose) {
@@ -82,7 +84,7 @@ function(has, mixin, event, uid, classList, timers, prompt, loader, i18n, fabric
 					 */
 					function clickEvent($event) {
 						var img, uploader, file,
-							element = $event.target,
+							element = BUTTON.findAncestor($event.target),
 							id = element.getAttribute("data-wc-selector");
 						if (id && element.localName === "button") {
 							uploader = document.getElementById(id);
@@ -505,7 +507,8 @@ function(has, mixin, event, uid, classList, timers, prompt, loader, i18n, fabric
 			event.add(container, "touchend", pressEnd);
 
 			function clickEvent($event) {
-				var element = $event.target,
+				var target = $event.target,
+					element = BUTTON.findAncestor(target),
 					config = getEventConfig(element, "click");
 				if (config) {
 					pressEnd();
@@ -526,7 +529,9 @@ function(has, mixin, event, uid, classList, timers, prompt, loader, i18n, fabric
 			}
 
 			function pressStart($event) {
-				var config = getEventConfig($event.target, "press");
+				var target = $event.target,
+					element = BUTTON.findAncestor(target),
+					config= getEventConfig(element, "press");
 				if (config) {
 					pressEnd();
 					timer = timers.setInterval(callbackWrapper, 100, config);

commit b8018621cfc305e757e027f059da7728b8d8e2ad

Fix redact mode (broken in #1139)

wcomponents-theme/src/main/js/wc/ui/imageEdit.js
@@ -524,9 +524,12 @@ function(has, mixin, Widget, event, uid, classList, timers, prompt, i18n, fabric
 			event.add(container, "touchend", pressEnd);
 
 			function clickEvent($event) {
-				var target = $event.target,
-					element = BUTTON.findAncestor(target),
+				var element = $event.target,
 					config = getEventConfig(element, "click");
+				if (!config) {
+					element = BUTTON.findAncestor(element);
+					config = getEventConfig(element, "click");
+				}
 				if (config) {
 					pressEnd();
 					timer = timers.setTimeout(config.func.bind(this, config, $event), 0);