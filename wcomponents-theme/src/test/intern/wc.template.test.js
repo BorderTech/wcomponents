95df85183adb8fd12112da35fd0de76a534bd0a1
Mark Reeves <mark@marksreeves.com>
2017-06-14 21:00:51 +1000

    Fix test JS issues found by eslint

--- wcomponents-theme/src/test/intern/wc.template.test.js
+++ wcomponents-theme/src/test/intern/wc.template.test.js
@@ -2,7 +2,7 @@ define(["intern!object", "intern/chai!assert", "./resources/test.utils!"],
 	function (registerSuite, assert, testutils) {
 		"use strict";
 
-		var template, testHolder, i = 0;
+		var template, testHolder; // , i = 0;
 
 //		function getNewKeyName() {
 //			return "key" + i++;
