3f158dd8d6c7a0a3a16bb56ec25590182bb3b990
Mark Reeves <mark@marksreeves.com>
2018-03-28 23:02:07 +1100

    A better way to generate the diagnostic IDs. They are only needed to populate the `aria-describedby` attribute on the Diagnosable so the value is unimportant.

--- wcomponents-core/src/main/java/com/github/bordertech/wcomponents/render/webxml/DiagnosticRenderUtil.java
+++ wcomponents-core/src/main/java/com/github/bordertech/wcomponents/render/webxml/DiagnosticRenderUtil.java
@@ -6,6 +6,7 @@ import com.github.bordertech.wcomponents.servlet.WebXmlRenderContext;
 import com.github.bordertech.wcomponents.util.SystemException;
 import com.github.bordertech.wcomponents.validation.Diagnostic;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Utility to render inline diagnostic messages.
@@ -22,26 +23,6 @@ public final class DiagnosticRenderUtil {
 	 * The xml element name used for each message in the diagnostic output.
 	 */
 	private static final String MESSAGE_TAG_NAME = "ui:message";
-
-	/**
-	 * Used to create an error diagnostic for a component. Value is unimportant so long as it is different from the other extensions.
-	 */
-	private static final String ERR_EXTENSION = "_err";
-
-	/**
-	 * Used to create a warning diagnostic for a component. Value is unimportant so long as it is different from the other extensions.
-	 */
-	private static final String WARN_EXTENSION = "_wrn";
-
-	/**
-	 * Used to create an info diagnostic for a component. Value is unimportant so long as it is different from the other extensions.
-	 */
-	private static final String INFO_EXTENSION = "_nfo";
-
-	/**
-	 * Used to create a success diagnostic for a component. Value is unimportant so long as it is different from the other extensions.
-	 */
-	private static final String SUCCESS_EXTENSION = "_scc";
 
 	/**
 	 * Prevent instantiation.
@@ -85,28 +66,9 @@ public final class DiagnosticRenderUtil {
 
 		XmlStringBuilder xml = renderContext.getWriter();
 
-		String id = component.getId();
-		String indicatorId = id;
-		switch (severity) {
-			case Diagnostic.ERROR:
-				indicatorId = indicatorId.concat(ERR_EXTENSION);
-				break;
-			case Diagnostic.WARNING:
-				indicatorId = indicatorId.concat(WARN_EXTENSION);
-				break;
-			case Diagnostic.INFO:
-				indicatorId = indicatorId.concat(INFO_EXTENSION);
-				break;
-			case Diagnostic.SUCCESS:
-				indicatorId = indicatorId.concat(SUCCESS_EXTENSION);
-				break;
-			default:
-				throw new SystemException("Unexpected diagnostic severity");
-		}
-		xml.appendTagOpen(TAG_NAME);
-		xml.appendAttribute("id", indicatorId);
+		xml.appendAttribute("id", UUID.randomUUID().toString());
 		xml.appendAttribute("type", getLevel(severity));
-		xml.appendAttribute("for", id);
+		xml.appendAttribute("for", component.getId());
 		xml.appendClose();
 
 		for (Diagnostic diagnostic : diags) {
@@ -123,13 +85,13 @@ public final class DiagnosticRenderUtil {
 	 * @param renderContext the RenderContext to paint to.
 	 */
 	public static void renderDiagnostics(final Diagnosable component, final WebXmlRenderContext renderContext) {
-		List<Diagnostic> diags = component.getDiagnostics(Diagnostic.WARNING);
-		if (diags != null) {
-			renderHelper(renderContext, component, diags, Diagnostic.WARNING);
-		}
-		diags = component.getDiagnostics(Diagnostic.ERROR);
+		List<Diagnostic> diags = component.getDiagnostics(Diagnostic.ERROR);
 		if (diags != null) {
 			renderHelper(renderContext, component, diags, Diagnostic.ERROR);
+		}
+		diags = component.getDiagnostics(Diagnostic.WARNING);
+		if (diags != null) {
+			renderHelper(renderContext, component, diags, Diagnostic.WARNING);
 		}
 		diags = component.getDiagnostics(Diagnostic.INFO);
 		if (diags != null) {
