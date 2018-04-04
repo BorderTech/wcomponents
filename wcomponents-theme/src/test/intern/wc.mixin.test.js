4b818ac36924d57add7366deb041eb61d225e707
Mark Reeves <mark@marksreeves.com>
2018-04-04 12:11:55 +1000

    Update mixin unit tests

--- wcomponents-theme/src/test/intern/wc.mixin.test.js
+++ wcomponents-theme/src/test/intern/wc.mixin.test.js
@@ -11,103 +11,105 @@ define(["intern!object", "intern/chai!assert", "./resources/test.utils!"],
 					mixin = obj;
 				});
 			},
-			testSimple: function() {
-				var source = {
-						foo: "bar",
-						sheep: {
-							species: "ovine",
-							noise: "baa"
-						}
-					},
-					target = {
-						foo: "kung",
-						num: 7,
-						sheep: {
-							noise: "maaaa",
-							flavour: "delicious"
-						}
-					},
-					expected = {
-						foo: "bar",
-						num: 7,
-						sheep: {
-							species: "ovine",
-							noise: "baa",
-							flavour: "delicious"
-						}
-					},
-					actual = mixin(source, target);
-				assert.deepEqual(actual, expected, "expected: " + JSON.stringify(expected) + ", but got: " + JSON.stringify(actual));
-			},
-			testNoTarget: function() {
-				var source = {
-						foo: "bar",
-						num: 7,
-						sheep: {
-							species: "ovine",
-							noise: "baa",
-							flavour: "delicious"
-						}
-					},
-					actual = mixin(source);
-				assert.deepEqual(actual, source);
-			},
-			testNoTargetNotSource: function() {
-				var source = {
-						foo: "bar",
-						num: 7,
-						sheep: {
-							species: "ovine",
-							noise: "baa",
-							flavour: "delicious"
-						}
-					},
-					actual = mixin(source);
-				assert.notEqual(actual, source);
-			},
-			testNoSourceReturnsTargetUnharmed: function() {
-				var target = {
-						foo: "bar",
-						num: 7,
-						sheep: {
-							species: "ovine",
-							noise: "baa",
-							flavour: "delicious"
-						}
-					},
-					actual = mixin(null, target);
-				assert.strictEqual(actual, target);
-			},
-			testNoSourceNoTarget: function() {
-				assert.isObject(mixin());
-			},
-			testNoSourceNoTargetRReturnsEmpty: function() {
-				assert.deepEqual(mixin(), {}); // want to use assert.isEmpty when we upgrade intern/chai
-			},
-			testShallow: function() {
-				var source = {
-						foo: "bar",
-						sheep: {
-							species: "ovine",
-							flavour: "delicious"
-						}
-					},
-					target = {
-						foo: "kung",
-						num: 7,
-						sheep: {
-							noise: "maaaa"
-						}
-					},
-					expected = {
-						foo: "bar",
-						sheep: {
-							species: "ovine",
-							flavour: "delicious"
+			tests : {
+				"simple": function() {
+					var source = {
+							foo: "bar",
+							sheep: {
+								species: "ovine",
+								noise: "baa"
+							}
 						},
-						num: 7
-					};
-				assert.deepEqual(mixin(source, target, true), expected);
+						target = {
+							foo: "kung",
+							num: 7,
+							sheep: {
+								noise: "maaaa",
+								flavour: "delicious"
+							}
+						},
+						expected = {
+							foo: "bar",
+							num: 7,
+							sheep: {
+								species: "ovine",
+								noise: "baa",
+								flavour: "delicious"
+							}
+						},
+						actual = mixin(source, target);
+					assert.deepEqual(actual, expected, "expected: " + JSON.stringify(expected) + ", but got: " + JSON.stringify(actual));
+				},
+				"no target": function() {
+					var source = {
+							foo: "bar",
+							num: 7,
+							sheep: {
+								species: "ovine",
+								noise: "baa",
+								flavour: "delicious"
+							}
+						},
+						actual = mixin(source);
+					assert.deepEqual(actual, source);
+				},
+				"no target doesn't return source": function() {
+					var source = {
+							foo: "bar",
+							num: 7,
+							sheep: {
+								species: "ovine",
+								noise: "baa",
+								flavour: "delicious"
+							}
+						},
+						actual = mixin(source);
+					assert.notEqual(actual, source);
+				},
+				"no source returns target unharmed": function() {
+					var target = {
+							foo: "bar",
+							num: 7,
+							sheep: {
+								species: "ovine",
+								noise: "baa",
+								flavour: "delicious"
+							}
+						},
+						actual = mixin(null, target);
+					assert.strictEqual(actual, target);
+				},
+				"no source, no target": function() {
+					assert.isObject(mixin());
+				},
+				"NoSourceNoTargetRReturnsEmpty": function() {
+					assert.deepEqual(mixin(), {}); // want to use assert.isEmpty when we upgrade intern/chai
+				},
+				"Shallow": function() {
+					var source = {
+							foo: "bar",
+							sheep: {
+								species: "ovine",
+								flavour: "delicious"
+							}
+						},
+						target = {
+							foo: "kung",
+							num: 7,
+							sheep: {
+								noise: "maaaa"
+							}
+						},
+						expected = {
+							foo: "bar",
+							sheep: {
+								species: "ovine",
+								flavour: "delicious"
+							},
+							num: 7
+						};
+					assert.deepEqual(mixin(source, target, true), expected);
+				}
 			}
 		});
 	});
