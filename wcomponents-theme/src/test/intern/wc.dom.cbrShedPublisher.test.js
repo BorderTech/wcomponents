e0c83afe11e04951f45687094217ab6bdb916867
Mark Reeves <mark@marksreeves.com>
2018-04-04 20:31:29 +1000

    New unit tests for `wc/dom/cbrShedPublisher` resulted in the discovery that most of the code in that module was to work around flaws in IE6-8 and no longer needed so I cleaned it up and wrote a complete test suite. This module used to be a silent dependency in wc/ui/checkBox, wc/ui/checkBoxSelect and wc/ui/radioButtonSelect but by making the dependency explicit we can reuse the Widgets resulting in less redundant code.

--- wcomponents-theme/src/test/intern/wc.dom.cbrShedPublisher.test.js
+++ wcomponents-theme/src/test/intern/wc.dom.cbrShedPublisher.test.js
@@ -1,5 +1,5 @@
-define(["intern!object", "intern/chai!assert", "./resources/test.utils!", "wc/dom/cbrShedPublisher", "wc/dom/shed", "wc/dom/event"],
-	function (registerSuite, assert, testutils, controller, shed, event) {
+define(["intern!object", "intern/chai!assert", "./resources/test.utils!", "wc/dom/cbrShedPublisher", "wc/dom/shed", "wc/dom/event", "wc/dom/Widget"],
+	function (registerSuite, assert, testutils, controller, shed, event, Widget) {
 		"use strict";
 
 		var testHolder,
@@ -9,40 +9,48 @@ define(["intern!object", "intern/chai!assert", "./resources/test.utils!", "wc/do
 <label id='l4' for='cb2'><input type='checkbox' id='cb2' name='cbg2' checked> cb two</label>",
 			done = false;
 
-
 		function subscriber() {
 			done = true;
 		}
 
-		function doClick(id) {
-			var element = document.getElementById(id);
-			if (!element) {
-				assert.isTrue(false, "no element to click");
+		// only need to focus the input element if the browser does not implement event capture phase
+		function testDoFocus(element) {
+			if (!event.canCapture) {
+				element.focus();
 			}
-			event.fire(element, event.TYPE.click);
 		}
 
 		function focusThenClick(id, otherId) {
 			var element = document.getElementById(id),
-				idToClick = otherId || id;
+				idToClick;
 			if (!element) {
 				assert.isTrue(false, "no element to focus");
 			}
-			event.fire(element, event.TYPE.focus);
-			doClick(idToClick);
+			testDoFocus(element);
+			idToClick = otherId || id;
+			element = document.getElementById(idToClick);
+			if (!element) {
+				assert.isTrue(false, "no element to click");
+			}
+			// event.fire(element, event.TYPE.click);
+			element.click();
 		}
 
 		registerSuite({
 			name: "wc/dom/cbrShedPublisher",
 			setup: function() {
 				testHolder = testutils.getTestHolder();
-				controller.initialise(testHolder);
+				testHolder.innerHTML = testContent;
 				shed.subscribe(shed.actions.SELECT, subscriber);
 				shed.subscribe(shed.actions.DESELECT, subscriber);
+				controller.initialise(testHolder);
 			},
 			beforeEach: function() {
 				testHolder.innerHTML = testContent;
 				done = false;
+			},
+			teardown: function() {
+				testHolder.innerHTML = "";
 			},
 			tests : {
 				"click checkbox to select publishes" : function() {
@@ -73,20 +81,78 @@ define(["intern!object", "intern/chai!assert", "./resources/test.utils!", "wc/do
 					focusThenClick("r2", "l2");
 					assert.isFalse(done);
 				},
-				"click checkbox without focus still publishes": function() {
-					doClick("cb1");
-					assert.isTrue(done);
-				},
 				"click disabled checkbox does not publish": function() {
 					var id = "cb1",
 						element = document.getElementById(id);
 					try {
+						testDoFocus(element); // focus (if required) before disabling to ensure changeEvent is wired up
 						shed.disable(element, true);
-						doClick(id);
+						element.click();
 						assert.isFalse(done);
 					} finally {
 						shed.enable(element, true);
+						element.click();
+						assert.isTrue(done); // nothing up my sleeves - this is to check the call to focus worked ok
 					}
+				},
+				"change event on checkbox publishes": function() {
+					var id = "cb1",
+						element = document.getElementById(id);
+					testDoFocus(element);
+					event.fire(element, event.TYPE.change);
+					assert.isTrue(done);
+				},
+				"change event on unchecked radio publishes": function() {
+					var id = "r1",
+						element = document.getElementById(id);
+					testDoFocus(element);
+					event.fire(element, event.TYPE.change);
+					assert.isTrue(done);
+				},
+				"change event on checked radio publishes": function() {
+					var id = "r2",
+						element = document.getElementById(id);
+					testDoFocus(element);
+					event.fire(element, event.TYPE.change);
+					assert.isTrue(done);
+				},
+				"change event on unfocussed checkbox does not publish if event cannot capture": function() {
+					if (event.canCapture) {
+						this.skip("test for non-capturing browsers only");
+					}
+					var id = "cb1",
+						element = document.getElementById(id);
+					event.fire(element, event.TYPE.change);
+					assert.isFalse(done);
+				},
+				"change event on unfocussed radio does not publish if event cannot capture": function() {
+					if (event.canCapture) {
+						this.skip("test for non-capturing browsers only");
+					}
+					var id = "r1",
+						element = document.getElementById(id);
+					event.fire(element, event.TYPE.change);
+					assert.isFalse(done);
+				},
+				"get radio widget": function() {
+					var w = controller.getWidget("r"),
+						element = document.getElementById("r1");
+					assert.isOk(w);
+					assert.isTrue(w.isOneOfMe(element));
+				},
+				"get checkbox widget": function() {
+					var w = controller.getWidget("cb"),
+						element = document.getElementById("cb1");
+					assert.isOk(w);
+					assert.isTrue(w.isOneOfMe(element));
+				},
+				"get widgets": function() {
+					var w = controller.getWidget(),
+						element = document.getElementById("cb1");
+					assert.isTrue(Array.isArray(w));
+					assert.isTrue(Widget.isOneOfMe(element, w), "Expected a check box to be a match for the Widget array");
+					element = document.getElementById("r1");
+					assert.isTrue(Widget.isOneOfMe(element, w), "Expected a radio button to be a match for the Widget array");
 				}
 			}
 		});
