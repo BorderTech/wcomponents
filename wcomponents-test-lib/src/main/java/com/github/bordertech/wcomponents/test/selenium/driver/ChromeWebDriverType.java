e0d71c2db19a86d1b8487c06d88d5fa2d3cd16c9
Rick Brown <ricksbrown73@gmail.com>
2020-05-13 11:03:24 +1000

    Upgrade to WebDriverManager 4.0.0
    This change allows WebDriverManager to be run behind a corporate proxy
    using Nexus Repository Manager as a mirror.

--- wcomponents-test-lib/src/main/java/com/github/bordertech/wcomponents/test/selenium/driver/ChromeWebDriverType.java
+++ wcomponents-test-lib/src/main/java/com/github/bordertech/wcomponents/test/selenium/driver/ChromeWebDriverType.java
@@ -1,6 +1,6 @@
 package com.github.bordertech.wcomponents.test.selenium.driver;
 
-import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
