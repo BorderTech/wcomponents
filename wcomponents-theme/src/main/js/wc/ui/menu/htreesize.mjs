9cd42daad17b4b30158e35cf83fc86cfdc66bbfc
Rick <ricksbrown73@gmail.com>
2023-08-24 15:24:22 +1000

    Port more modules off requirejs

---
+++ wcomponents-theme/src/main/js/wc/ui/menu/htreesize.mjs
@@ -0,0 +1,188 @@
+import event from "wc/dom/event";
+import initialise from "wc/dom/initialise";
+import tree from "wc/ui/menu/tree";
+import resizeable from "wc/ui/resizeable";
+import storage from "wc/dom/storage";
+import processResponse from "wc/ui/ajax/processResponse";
+import attribute from "wc/dom/attribute";
+
+const handleSelector = ".wc_branch_resize_handle";
+/**
+ * @constructor
+ * @alias module:wc/ui/menu/htreesize~TreeSizer
+ * @private
+ */
+function TreeSizer() {
+	const resized = {},
+		STORE_KEY = "wc-treesize";
+
+	/**
+	 * When a tree component is resized store the preferred size for later use.  A subscriber for
+	 * {@link module:wc/ui/resizeable}.
+	 *
+	 * @function
+	 * @private
+	 * @param {HTMLElement} element The element which was resized.
+	 */
+	function resizeSubscriber(element) {
+		if (element) {
+			let store;
+			const { id, style} = element;
+
+			if (style.width || style.height) {
+				resized[id] = { width: style.width, height: style.height} ;
+				store = true;
+			} else if (resized[id]) {
+			} else if (resized[id]) {
+				delete resized[id];
+				store = true;
+			}
+
+			if (store) {
+				storage.put(STORE_KEY, JSON.stringify(resized));
+			}
+		}
+	}
+
+	/**
+	 * Set height and width on segments of a htree if these were stored previously.
+	 *
+	 * @function
+	 * @private
+	 * @param {String} id The id of the component to be sized.
+	 * @param {module:wc/ui/menu/htreesize~dto} obj A dto containing height and/or width.
+	 */
+	function doReapplySize(id, obj) {
+		const element = document.getElementById(id);
+		if (element && tree.isHTree(tree.getRoot(element))) {
+
+			const { width, height } = obj;
+
+			if (width) {
+				element.style.width = width;
+			}
+			if (height) {
+				element.style.height = height;
+			}
+
+			if (width || height) {
+				resized[id] = { width, height };
+			}
+		}
+	}
+
+	/**
+	 * An iterator function to set sizes on groups in a htree.
+	 * @function
+	 * @private
+	 * @param {Element} element The tree or group being adjusted.
+	 * @param {Object} stored The object version of the stored size info.
+	 */
+	function reapplySizeOnTreeGroup(element, stored) {
+		const id = element.id;
+		const obj = stored ? stored[id] : null;
+		if (obj) {
+			doReapplySize(id, obj);
+		}
+
+	}
+
+	/**
+	 * Apply user's previously stored sizes if htree or group is inserted via ajax.
+	 * @function
+	 * @private
+	 * @param {Element} element The ajax target.
+	 */
+	function ajaxSubscriber(element) {
+		let stored = storage.get(STORE_KEY);
+		if (stored) {
+			stored = JSON.parse(stored);
+
+			if ((tree.isSubMenu(element) && tree.isHTree(tree.getRoot(element)))) {
+				reapplySizeOnTreeGroup(element, stored);
+			}
+
+			const submenus = element.querySelectorAll(tree._wd.submenu.toString());
+			Array.from(submenus).forEach(function(next) {
+				if (tree.isHTree(tree.getRoot(next))) {
+					reapplySizeOnTreeGroup(next, stored);
+				}
+			});
+		}
+	}
+
+	/**
+	 * Reset size on doubleclick on the handle.
+	 * @function
+	 * @private
+	 * @param {MouseEvent} $event A dblclick event.
+	 */
+	function dblClickEvent($event) {
+		const { target, defaultPrevented } = $event;
+		if (!defaultPrevented) {
+			const element = target.closest(handleSelector);
+			if (element) {
+				if (resizeable.clearSize(element)) {
+					$event.preventDefault();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Handle key down.
+	 * @param {KeyboardEvent} $event
+	 */
+	function keydownEvent($event) {
+		if (!$event.defaultPrevented && $event.key === "Enter") {
+			if (resizeable.clearSize($event.target)) {
+				$event.preventDefault();
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param {FocusEvent} $event
+	 */
+	function focusEvent($event) {
+		const { target } = $event;
+		const BS = "htreesize.inited";
+		if (target.matches(handleSelector) && !attribute.get(target, BS)) {
+			event.add(target, "keydown", keydownEvent);
+			attribute.set(target, BS, true);
+		}
+	}
+
+	/**
+	 * initialise htrees by setting any stored sizes and registering subscribers.
+	 *
+	 * @function module:wc/ui/menu/htreesize.initialise
+	 * @public
+	 * @param {Element} element The element being initialised.
+	 */
+	this.initialise = function(element) {
+		event.add(element, { type: "focus", listener: focusEvent, capture: true });
+
+		event.add(element, "dblclick", dblClickEvent);
+		processResponse.subscribe(ajaxSubscriber, true);
+		resizeable.subscribe(resizeSubscriber);
+		let stored = storage.get(STORE_KEY);
+		if (stored) {
+			stored = JSON.parse(stored);
+			for (let o in stored) {
+				doReapplySize(o, stored[o]);
+			}
+		}
+	};
+}
+
+export default initialise.register(new TreeSizer());
+
+/**
+ * @typedef {Object} module:wc/ui/menu/htreesize~dto An object which stores information about a htree or a htree
+ * submenu.
+ * @property {String} [height] The CSS height of the item including units.
+ * @property {String} [width] The CSS width of the item including units.
+ */
+
