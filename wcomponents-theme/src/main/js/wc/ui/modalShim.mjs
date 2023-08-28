7fdbe6a55711baef4ae7fbcc9880ecc26263d1a5
Rick <ricksbrown73@gmail.com>
2023-08-28 17:38:27 +1000

    Ported more modules

--- wcomponents-theme/src/main/js/wc/ui/modalShim.mjs
+++ wcomponents-theme/src/main/js/wc/ui/modalShim.mjs
@@ -1,132 +1,26 @@
-import attribute from "wc/dom/attribute";
 import uid from "wc/dom/uid";
 import event from "wc/dom/event";
 import focus from "wc/dom/focus";
-import Widget from "wc/dom/Widget";
 import shed from "wc/dom/shed";
 import timers from "wc/timers";
 import Observer from "wc/Observer";
 
-export default new ModalShim();
+const MODAL_BACKGROUND_ID = "wc-shim";
+const UNIT = "px";
+const accessKeySelector = "[accesskey]";
+const AKEY = "accesskey";
+const ALTERNATE_OBSERVER_GROUP = "shimshow";
 
+let SHIFTKEY_ON = false;
+let accessKeyMap = {};
+let activeElement;
+let observer;
 
 /**
  * Used to overlay the page with a shim which blocks mouse access to the components on the page and steals focus back
- *    if it loses it (e.g. due to TAB key or accesskeys).
- * @constructor
- * @alias module:wc/ui/modalShim~ModalShim
- * @private
+ *    if it loses it (e.g. due to TAB key or access keys).
  */
-function ModalShim() {
-	const MODAL_BACKGROUND_ID = "wc-shim";
-	const UNIT = "px";
-	const ACCESS_KEY_WD = new Widget("", "", { accesskey: null });
-	const AKEY = "accesskey";
-	const ALTERNATE_OBSERVER_GROUP = "shimshow";
-
-	let SHIFTKEY_ON = false;
-	let accessKeyMap = {};
-	let activeElement;
-	let observer;
-
-	/**
-	 * If the user is shift-tabbing their way back through the dialog we want to wrap focus around to the last
-	 * tabstop, not the first. To do this we need to track the state of the shift key because focus event does
-	 * not report shift key flag.
-	 *
-	 * @function
-	 * @private
-	 * @param {KeyboardEvent} $event The keydown event.
-	 */
-	function keyEvent($event) {
-		SHIFTKEY_ON = $event.shiftKey;
-	}
-
-	/**
-	 * Returns truthy if the container contains, or is identical to, the element.
-	 * @param container The candidate for container.
-	 * @param element The candidate for contained.
-	 * @returns truthy if container contains, or is, element.
-	 */
-	function elementContains(container, element) {
-		let result = container === element;
-		// noinspection JSBitwiseOperatorUsage
-		return result || (element.compareDocumentPosition(container) & Node.DOCUMENT_POSITION_CONTAINS);
-	}
-
-	/**
-	 * The activeElement (or something within it) has received focus. Cancel any outstanding calls to refocus it.
-	 * @function
-	 * @private
-	 * @param {KeyboardEvent} $event The focus event.
-	 */
-	function focusEvent($event) {
-		if (!$event.defaultPrevented && activeElement) {
-			if (!elementContains(activeElement, $event.target)) {
-				timers.setTimeout(focus.focusFirstTabstop, 0, activeElement, null, SHIFTKEY_ON);
-			}
-		}
-	}
-
-	/**
-	 * Use the shim to prevent touch events by cancelling touchstart.
-	 * @function
-	 * @private
-	 * @param {TouchEvent} $event The touch start event.
-	 */
-	function touchstartEvent($event) {
-		if (!$event.defaultPrevented && activeElement &&
-			!elementContains(activeElement, $event.target)) {
-			$event.preventDefault();
-		}
-	}
-
-	/**
-	 * create a new modalShim if required.
-	 * @function
-	 * @private
-	 * @returns {Element} The shim element.
-	 */
-	function create() {
-		const d = document,
-			b = d.body,
-			result = d.createElement("div");
-		result.id = MODAL_BACKGROUND_ID;
-		shed.hide(result, true);
-		if (b.firstChild) {
-			b.insertBefore(result, b.firstChild);
-		} else {
-			b.appendChild(result);
-		}
-		return result;
-	}
-
-	/**
-	 * Attaches or detaches the events required by the modal shim.
-	 * @param {Boolean} [add] true to add the events, otherwise remove them.
-	 */
-	function addRemoveEvents(add) {
-		const HAS_EVENTS = "wc/ui/modalShim.wired";
-		const element = document.body;
-		if (!(add && attribute.get(element, HAS_EVENTS))) {
-			const action = add ? "add" : "remove";
-			if (add) {
-				attribute.set(element, HAS_EVENTS, true);
-			} else {
-				attribute.remove(element, HAS_EVENTS);
-			}
-			event[action](element, "keydown", keyEvent, false);
-			event[action](element, "keyup", keyEvent, false);
-			if (add) {
-				event[action](element, "focus", focusEvent, null, null, true);
-				event[action](document.body, "touchstart", touchstartEvent, null, null, true);
-			} else {
-				event[action](element, "focus", focusEvent, true);
-				event[action](document.body, "touchstart", touchstartEvent, true);
-			}
-		}
-	}
-
+const instance = {
 	/**
 	 * Prevents interaction with the current browser window except for the active region.
 	 * @function module:wc/ui/modalShim.setModal
@@ -134,7 +28,7 @@ function ModalShim() {
 	 *    (i.e. no interaction allowed).
 	 * @param {String} [className] Additional class to add to the shim.
 	 */
-	this.setModal = function(activeRegion, className) {
+	setModal: function(activeRegion, className) {
 		const shimElement = document.getElementById(MODAL_BACKGROUND_ID) || create();
 		shimElement.style.height = document.documentElement.scrollHeight + UNIT;
 		shed.show(shimElement, true);
@@ -145,7 +39,7 @@ function ModalShim() {
 		}
 
 		// remove the accesskey attribute from controls with access keys which are not in the activeRegion
-		Array.prototype.forEach.call(ACCESS_KEY_WD.findDescendants(document), function(next) {
+		Array.from(document.querySelectorAll(accessKeySelector)).forEach(next => {
 			const nextId = next.id || (next.id = uid());
 			if (activeRegion && !elementContains(activeRegion, next)) {
 				accessKeyMap[nextId] = next.getAttribute(AKEY);
@@ -157,13 +51,13 @@ function ModalShim() {
 			observer.setFilter(ALTERNATE_OBSERVER_GROUP);
 			observer.notify(activeElement);
 		}
-	};
+	},
 
 	/**
 	 * Removes the modal shim.
 	 * @function module:wc/ui/modalShim.clearModal
 	 */
-	this.clearModal = function() {
+	clearModal: function() {
 		let notify;
 		try {
 			const shimElement = document.getElementById(MODAL_BACKGROUND_ID);
@@ -186,7 +80,7 @@ function ModalShim() {
 				observer.notify();
 			}
 		}
-	};
+	},
 
 	/**
 	 * Allow external module to subscribe to this module's Observer instance to be informed when a modal shim is removed.
@@ -202,7 +96,7 @@ function ModalShim() {
 	 * @param {boolean} onshow if true notify when the modalShim is shown, otherwise notify when the shim is removed
 	 * @returns {Function} the subscribed function
 	 */
-	this.subscribe = function(subscriber, onshow) {
+	subscribe: function(subscriber, onshow) {
 		let group = null;
 		if (!observer) {
 			observer = new Observer();
@@ -211,7 +105,7 @@ function ModalShim() {
 			group = { group: ALTERNATE_OBSERVER_GROUP };
 		}
 		return observer.subscribe(subscriber, group);
-	};
+	},
 
 	/**
 	 * Unsubscribe from this observer instance.
@@ -222,11 +116,107 @@ function ModalShim() {
 	 * the group is the same as when the subscriber was subscribed.
 	 * @returns {Function} the unsubscribed function
 	 */
-	this.unsubscribe = function(subscriber, onshow) {
+	unsubscribe: function(subscriber, onshow) {
 		if (observer) {
 			const group = onshow ? ALTERNATE_OBSERVER_GROUP : null;
 			return observer.unsubscribe(subscriber, group);
 		}
 		return null;
-	};
+	}
+};
+
+/**
+ * If the user is shift-tabbing their way back through the dialog we want to wrap focus around to the last
+ * tabstop, not the first. To do this we need to track the state of the shift key because focus event does
+ * not report shift key flag.
+ *
+ * @function
+ * @private
+ * @param {KeyboardEvent} $event The keydown event.
+ */
+function keyEvent($event) {
+	SHIFTKEY_ON = $event.shiftKey;
 }
+
+/**
+ * Returns truthy if the container contains, or is identical to, the element.
+ * @param container The candidate for container.
+ * @param element The candidate for contained.
+ * @returns truthy if container contains, or is, element.
+ */
+function elementContains(container, element) {
+	let result = container === element;
+	// noinspection JSBitwiseOperatorUsage
+	return result || (element.compareDocumentPosition(container) & Node.DOCUMENT_POSITION_CONTAINS);
+}
+
+/**
+ * The activeElement (or something within it) has received focus. Cancel any outstanding calls to refocus it.
+ * @function
+ * @private
+ * @param {KeyboardEvent} $event The focus event.
+ */
+function focusEvent($event) {
+	if (!$event.defaultPrevented && activeElement) {
+		if (!elementContains(activeElement, $event.target)) {
+			timers.setTimeout(focus.focusFirstTabstop, 0, activeElement, null, SHIFTKEY_ON);
+		}
+	}
+}
+
+/**
+ * Use the shim to prevent touch events by cancelling touchstart.
+ * @function
+ * @private
+ * @param {TouchEvent} $event The touch start event.
+ */
+function touchstartEvent($event) {
+	if (!$event.defaultPrevented && activeElement &&
+		!elementContains(activeElement, $event.target)) {
+		$event.preventDefault();
+	}
+}
+
+/**
+ * create a new modalShim if required.
+ * @function
+ * @private
+ * @returns {Element} The shim element.
+ */
+function create() {
+	const d = document,
+		b = d.body,
+		result = d.createElement("div");
+	result.id = MODAL_BACKGROUND_ID;
+	shed.hide(result, true);
+	if (b.firstChild) {
+		b.insertBefore(result, b.firstChild);
+	} else {
+		b.appendChild(result);
+	}
+	return result;
+}
+
+/**
+ * Attaches or detaches the events required by the modal shim.
+ * @param {Boolean} [add] true to add the events, otherwise remove them.
+ */
+function addRemoveEvents(add) {
+	const HAS_EVENTS = "wc/ui/modalShim.wired";
+	const element = document.body;
+	if (!(add && element[HAS_EVENTS])) {
+		const action = add ? "add" : "remove";
+		element[HAS_EVENTS] = add;
+		event[action](element, "keydown", keyEvent, false);
+		event[action](element, "keyup", keyEvent, false);
+		if (add) {
+			event[action](element, "focus", focusEvent, null, null, true);
+			event[action](document.body, "touchstart", touchstartEvent, null, null, true);
+		} else {
+			event[action](element, "focus", focusEvent, true);
+			event[action](document.body, "touchstart", touchstartEvent, true);
+		}
+	}
+}
+
+export default instance;
--- wcomponents-theme/src/main/js/wc/ui/tooltip.mjs
+++ wcomponents-theme/src/main/js/wc/ui/tooltip.mjs
@@ -3,105 +3,101 @@ import initialise from "wc/dom/initialise";
 import shed from "wc/dom/shed";
 import timers from "wc/timers";
 
+const events = [],
+	tooltipsSelector = "span[role='tooltip']",
+	TOOLTIP_TTL = 5000;
+
+let showing,
+	isOpen = false;
+
 /**
  * Provides a mechanism to expose access keys in the UI. This is done using a little "tooltip" style element which
  * is shown when the ALT/META key is pressed.
- * @constructor
- * @alias module:wc/ui/tooltip~Tooltip
- * @private
  */
-function Tooltip() {
-	const events = [],
-		tooltipsSelector = "span[role='tooltip']",
-		TOOLTIP_TTL = 5000;
-
-	let showing,
-		isOpen = false;
-
-	/*
-	 * Manages scheduled cleanups of any tooltips being shown - they will be
-	 * automatically hidden in n seconds unless another routine has hidden
-	 * them before that time.
-	 */
-	function scheduledCleanup(hiding) {
-		if (hiding) {  // someone else is hiding the tooltips so cancel cleanup
-			if (showing) {
-				timers.clearTimeout(showing);
-				showing = null;
-			}
-		} else {  // someone is (re)showing tooltips, (re)schedule a cleanup
-			if (showing) {
-				timers.clearTimeout(showing);
-			}
-			showing = timers.setTimeout(toggleTooltips, TOOLTIP_TTL, true);
-		}
-	}
-
-	function toggleTooltips(hide) {
-		const tooltips = document.querySelectorAll(tooltipsSelector),
-			showHide = hide ? "hide" : "show";
-
-		isOpen = !hide;
-		for (const next of tooltips) {
-			let nextId = next.id;
-			let control = document.getElementById(nextId.slice(0, nextId.indexOf("_wctt")));
-			if (!control || (control && !shed.isDisabled(control))) {
-				shed[showHide](next, true);
-			}
-		}
-		scheduledCleanup(hide);
-	}
-
+const instance = {
 	/**
-	 * Handles a key down.
-	 * @param {KeyboardEvent} $event
+	 * Get a toolTip from an element.
+	 * @function module:wc/ui/tooltip.getTooltip
+	 * @param {HTMLElement} element An HTML element which may contain a toolTip.
+	 * @returns {HTMLElement} A toolTip element.
 	 */
-	function keydownEvent($event) {
-		if (!$event.defaultPrevented && !isOpen && $event.key === "Alt" && !($event.repeat)) {
-			isOpen = true;
-			toggleTooltips(false);
+	getTooltip: element => element.querySelector(tooltipsSelector)
+};
+
+/*
+ * Manages scheduled cleanups of any tooltips being shown - they will be
+ * automatically hidden in n seconds unless another routine has hidden
+ * them before that time.
+ */
+function scheduledCleanup(hiding) {
+	if (hiding) {  // someone else is hiding the tooltips so cancel cleanup
+		if (showing) {
+			timers.clearTimeout(showing);
+			showing = null;
+		}
+	} else {  // someone is (re)showing tooltips, (re)schedule a cleanup
+		if (showing) {
+			timers.clearTimeout(showing);
+		}
+		showing = timers.setTimeout(toggleTooltips, TOOLTIP_TTL, true);
+	}
+}
+
+function toggleTooltips(hide) {
+	const tooltips = document.querySelectorAll(tooltipsSelector),
+		showHide = hide ? "hide" : "show";
+
+	isOpen = !hide;
+	for (const next of tooltips) {
+		let nextId = next.id;
+		let control = document.getElementById(nextId.slice(0, nextId.indexOf("_wctt")));
+		if (!control || (control && !shed.isDisabled(control))) {
+			shed[showHide](next, true);
 		}
 	}
+	scheduledCleanup(hide);
+}
 
-	/* NOTE IE8 does not fire keyup when releasing ALT if there is a previous keyup (such as releasing the accesskey key) and if the browser
-	 * native accesskey functionality (for menus) has been invoked.
-	 */
-	/**
-	 * Handles a key up.
-	 * @param {KeyboardEvent} $event
-	 */
-	function keyupEvent($event) {
-		if (isOpen && !$event.defaultPrevented) {
-			toggleTooltips(true);
-		}
+/**
+ * Handles a key down.
+ * @param {KeyboardEvent} $event
+ */
+function keydownEvent($event) {
+	if (!$event.defaultPrevented && !isOpen && $event.key === "Alt" && !($event.repeat)) {
+		isOpen = true;
+		toggleTooltips(false);
 	}
+}
 
+/* NOTE IE8 does not fire keyup when releasing ALT if there is a previous keyup (such as releasing the accesskey key) and if the browser
+ * native accesskey functionality (for menus) has been invoked.
+ */
+/**
+ * Handles a key up.
+ * @param {KeyboardEvent} $event
+ */
+function keyupEvent($event) {
+	if (isOpen && !$event.defaultPrevented) {
+		toggleTooltips(true);
+	}
+}
+
+
+initialise.register({
 	/**
 	 * Initialise the tooltips by attaching necessary event listeners.
 	 * @function module:wc/ui/tooltip.initialise
 	 * @param {Element} element The element being initialised, usually document.body.
 	 */
-	this.initialise = function(element) {
+	initialise: element => {
 		events.push(event.add(element, "keydown", keydownEvent));
 		events.push(event.add(element, "keyup", keyupEvent));
-	};
+	},
 
 	/**
 	 * Unsubscribes event listeners etc.
 	 */
-	this.deinit = function() {
-		event.remove(events);
-	};
+	deinit: () => event.remove(events)
+});
 
-	/**
-	 * Get a toolTip from an element.
-	 * @function module:wc/ui/tooltip.getTooltip
-	 * @param {Element} element An HTML element which may contain a toolTip.
-	 * @returns {Element} A toolTip element.
-	 */
-	this.getTooltip = function(element) {
-		return element.querySelector(tooltipsSelector);
-	};
-}
-
-export default initialise.register(new Tooltip());
+export default instance;
