c6e7ba36efe4000691355d016ea7c6c9d8a965e1
Mark Reeves <mark.reeves@homeaffairs.gov.au>
2026-08-07 13:21:27 +1000

    Revert example change

--- wcomponents-examples/src/main/java/com/github/bordertech/wcomponents/examples/WDropdownOptionsExample.java
+++ wcomponents-examples/src/main/java/com/github/bordertech/wcomponents/examples/WDropdownOptionsExample.java
@@ -120,12 +120,6 @@ public class WDropdownOptionsExample extends WContainer {
 	 * disabled control.
 	 */
 	private final WCheckBox cbDisabled = new WCheckBox();
-    
-    /**
-     * read-only control
-     */
-	private final WCheckBox cbReadOnly = new WCheckBox();
-    
 	/**
 	 * action on change on/off checkbox.
 	 */
@@ -196,7 +190,6 @@ public class WDropdownOptionsExample extends WContainer {
 		layout.addField("Submit on change", cbSubmitOnChange);
 		layout.addField("Visible", cbVisible);
 		layout.addField("Disabled", cbDisabled);
-        layout.addField("ReadOnly", cbReadOnly);
 
 		// Apply Button
 		WButton apply = new WButton("Apply");
@@ -275,7 +268,6 @@ public class WDropdownOptionsExample extends WContainer {
 		// set misc options.
 		dropdown.setVisible(cbVisible.isSelected());
 		dropdown.setDisabled(cbDisabled.isSelected());
-        dropdown.setReadOnly(cbReadOnly.isSelected());
 
 		// add the action for action on change, ajax and subordinate.
 		if (cbActionOnChange.isSelected() || cbAjax.isSelected() || cbSubmitOnChange.isSelected()) {
